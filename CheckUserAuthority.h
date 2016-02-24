8d6a396ada89c81bde257890850f4a4f666d5284
baiyidjp <tztddong@dongjiangpengdeMac-mini.local>
2016-02-24 17:21:19 +0800

    上传

---
+++ CheckUserAuthority.h
@@ -0,0 +1,24 @@
+//
+//  CheckUserAuthority.h
+//  KaixinketangForTeacher
+//
+//  Created by tztddong on 16/2/24.
+//  Copyright © 2016年 TZTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CheckUserAuthority : NSObject
+/**
+ *  检查是否有使用相机的权限
+ */
++ (BOOL)checkCameraAuthority;
+/**
+ *  检查是否有使用照片的权限
+ */
++ (BOOL)checkImageAuthority;
+/**
+ *  检查是否有使用麦克风的权限
+ */
++ (BOOL)checkRecordAuthority;
+@end
---
+++ CheckUserAuthority.m
@@ -0,0 +1,48 @@
+//
+//  CheckUserAuthority.m
+//  KaixinketangForTeacher
+//
+//  Created by tztddong on 16/2/24.
+//  Copyright © 2016年 TZTD. All rights reserved.
+//
+
+#import "CheckUserAuthority.h"
+#import <AVFoundation/AVCaptureDevice.h>
+#import <AVFoundation/AVMediaFormat.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <AVFoundation/AVAudioSession.h>
+@implementation CheckUserAuthority
+
+
++ (BOOL)checkCameraAuthority{
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    if (authStatus == AVAuthorizationStatusAuthorized){
+        return YES;
+    }else{
+        return NO;
+    }
+}
++ (BOOL)checkImageAuthority{
+    ALAuthorizationStatus author = [ALAssetsLibrary authorizationStatus];
+    if (author == ALAuthorizationStatusAuthorized){
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
++ (BOOL)checkRecordAuthority{
+    
+    __block BOOL bCanRecord = YES;
+    if ([[[UIDevice currentDevice] systemVersion] compare:@"7.0"] != NSOrderedAscending)
+    {
+        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+        if ([audioSession respondsToSelector:@selector(requestRecordPermission:)]) {
+            [audioSession performSelector:@selector(requestRecordPermission:) withObject:^(BOOL granted) {
+                bCanRecord = granted;
+            }];
+        }
+    }
+    return bCanRecord;
+}
+@end
